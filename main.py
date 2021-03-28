a6c493466681bcda1f15ac1d354c9537ee1fa6d8
gbelbe <gbelbe@gmail.com>
2021-03-28 15:32:08 +0200

    Retrieve list of annonces from site Kerentree immobilier pont l'abbée with date, description and link

--- main.py
+++ main.py
@@ -18,21 +18,29 @@ soup = BeautifulSoup(website.content, 'html.parser')
 
 #all_div_tags = soup.find_all(name = "div", class_ ="span12 padding-10 transition-bg recherche-annonces box-shadow")
 
-div_tag = soup.find(name = "div", class_ ="span12 padding-10 transition-bg recherche-annonces box-shadow")
+div_tag = soup.find_all(name="div", class_="span12 padding-10 transition-bg recherche-annonces box-shadow")
+
+
 #print(div_tag)
 
 ####WIP
 #desc = div_tag.find(name = "span")
 #print(span.get)
 
+for tag in div_tag:
 
-lien = div_tag.find(name= "a")
+    print("_______________________")
 
+    MAJ = tag.find("p", class_="margin-top-10 height-50").find_all(text=True, recursive=False)
+    #print(MAJ)
 
-print(lien.get("href"))
-print(lien.get("title"))
-print("_______________________")
-print(lien)
+    #retrieve the date, 2nd element of the text list received from MAJ
+    print(MAJ[1].strip())
+
+    lien = tag.find(name= "a")
+    print(lien.get("href"))
+    print(lien.get("title"))
+    #break
 
 
 #for tag in all_div_tags:
