8bc167e07afa8a3480ffbeb0a989026000df4075
gbelbe <gbelbe@gmail.com>
2021-04-12 09:24:31 +0200

    Collect all property element in the annonce_dict dictionary, then create a list called annonces_list with all properties parsed as annonce_dict, then finally order the annonces_list according to date of insertion (dict property)

--- main.py
+++ main.py
@@ -1,62 +1,60 @@
-#beautiful soup html parser library
-from bs4 import BeautifulSoup
-
-#requests import curl type library
-import requests
-
-#convert scraped date text to proper date format
+# convert scraped date text to proper date format
 from datetime import datetime
 
-#retrieve the url from the web address
-website = requests.get('http://www.kerentree-immobilier.fr/recherche,basic.htm?idp=568148&idtt=2&idtypebien=2')
+# requests import curl type library and beautiful soup html parser library
+import requests
+from bs4 import BeautifulSoup
 
-#check if status is ok http200
+# import regular expressions
+import re
+
+
+# retrieve the url from the web address
+website = requests.get('http://www.kerentree-immobilier.fr/recherche,basic.htm?idp=568148&idtt=2&idtypebien=2')
+# check if status is ok http200
 print(website)
 
 soup = BeautifulSoup(website.content, 'html.parser')
-#print(soup.prettify())
 
-#print(soup.find_all(name="div" ))
+# Grab all div tags with ID starting with "annonceXXX", that is showing the recap infos from the houses
+div_tag = soup.find_all(name="div", id=re.compile('^annonce*'))
 
-
-#all_div_tags = soup.find_all(name = "div", class_ ="span12 padding-10 transition-bg recherche-annonces box-shadow")
-
-div_tag = soup.find_all(name="div", class_="span12 padding-10 transition-bg recherche-annonces box-shadow")
-
-
-#print(div_tag)
-
-####WIP
-#desc = div_tag.find(name = "span")
-#print(span.get)
-
-annonces = []
+annonces_list = []
 
 for tag in div_tag:
 
-    print("_______________________")
+    annonce_dict = {}
+
+    id_annonce = tag.find(name="div", class_="btn btn-important btn-block bouton-ajouter-selection-detail btn-tooltip not-log")["data-idannonce"]
+    annonce_dict["id"]=id_annonce
+
+    img = tag.find("img")
+    # print(img['src'])
+
+    # annonce_dict["img"] = img['src']
+    # print(annonce_dict["img"])
+
+    # annonce_dict["long_desc"] = img['longdesc']
 
     MAJ = tag.find("p", class_="margin-top-10 height-50").find_all(text=True, recursive=False)
-    #print(MAJ)
-
-    #retrieve the date, 2nd element of the text list received from MAJ
+    # print(MAJ)
+    # retrieve the date, 2nd element of the text list received from MAJ
     majdate = datetime.strptime(MAJ[1].strip().split()[2], '%d/%m/%Y')
-    print(majdate)
-    annonces.append(majdate)
+    #print(majdate)
+    annonce_dict["maj_date"] = majdate
 
+    lien = tag.find(name="a")
+    url = lien.get("href")
+    titre = lien.get("title")
 
+    # annonce_dict["url"] = url
+    # annonce_dict["titre"] = titre
 
-    lien = tag.find(name= "a")
-    print(lien.get("href"))
-    print(lien.get("title"))
-    #break
+    annonces_list.append(annonce_dict)
 
-print(annonces)
+# We order the list by its most recent element: we must order a list with nested dictionary by one elem of the dict
+sorted_annonces = sorted(annonces_list, key=lambda k: k['maj_date'], reverse=True)
 
-#retrieve index position of the most recent item in the list annonces
-#max gives the most recent date when list elements are datetime
-print(annonces.index(max(annonces)))
-
-#for tag in all_div_tags:
- #   print(tag.getText)
+print(annonces_list)
+print(sorted_annonces)
 
