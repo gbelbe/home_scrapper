0b935a3b0d53070aae2c1f0f42734793f9a49eec
gbelbe <gbelbe@gmail.com>
2021-04-12 11:06:12 +0200

    re organize the project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name ='homescrapper',
+    name='homescrapper',
     extras_require=dict(tests=['pytest']),
     packages=find_packages(where='src'),
     package_dir={"": "src"}
