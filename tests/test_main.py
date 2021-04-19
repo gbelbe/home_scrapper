0f4576fc92182a85aee2359ff23b0d3d9feee49c
gbelbe <gbelbe@gmail.com>
2021-04-19 18:28:17 +0200

    Refactor test_main.py to reflect Class change

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,20 @@
 import sys
 sys.path.insert(0, '../src/homescrapper/')
 
-from homescrapper.main import site_parse
+from homescrapper.main import ParseSite
 
 def test_site_parse():
     site_url = "http://www.kerentree-immobilier.fr/recherche,basic.htm?idp=568148&idtt=2&idtypebien=2"
-    annonce_tags = {
-        "main_tag": "div",
-        "id_regular_exp": "^annonce*",
-        "inner_tag": "div",
-        "inner_class": "btn btn-important btn-block bouton-ajouter-selection-detail btn-tooltip not-log",
-    }
+    # annonce_tags = {
+    #     "main_tag": "div",
+    #     "id_regular_exp": "^annonce*",
+    #     "inner_tag": "div",
+    #     "inner_class": "btn btn-important btn-block bouton-ajouter-selection-detail btn-tooltip not-log",
+    # }
+    div = "btn btn-important btn-block bouton-ajouter-selection-detail btn-tooltip not-log"
+    regex = "^annonce*"
 
-    a = site_parse(site_url, annonce_tags)
-    assert len(a) != 0, 'the list is empty'
+    a = ParseSite(site_url, div, regex)
+    assert len(a.site_parse()) != 0, 'the list is empty'
+    # assert len(a) != 0, 'the list is empty'
 
